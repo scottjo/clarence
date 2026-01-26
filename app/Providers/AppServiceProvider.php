06204587e0e2ae361e538d4ce50b970ea9aca515
jon <jon_scott@me.com>
2026-01-26 18:24:15 +0000

    Add health checks to monitor application and infrastructure metrics
    - Integrated additional health checks for cache, database connections, database size, Horizon, Redis, Redis memory usage, and schedule heartbeat.
    - Configured thresholds for database connections, database size, and Redis memory usage.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\Health\Checks\Checks\CacheCheck;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
 use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
+use Spatie\Health\Checks\Checks\RedisMemoryUsageCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
 use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
 use Spatie\Health\Facades\Health;
 
@@ -31,6 +38,17 @@ class AppServiceProvider extends ServiceProvider
             EnvironmentCheck::new(),
             DatabaseCheck::new(),
             UsedDiskSpaceCheck::new(),
+            CacheCheck::new(),
+            DatabaseCheck::new(),
+            DatabaseConnectionCountCheck::new()
+                ->failWhenMoreConnectionsThan(100),
+            DatabaseSizeCheck::new()
+                ->failWhenSizeAboveGb(errorThresholdGb: 6.0),
+            HorizonCheck::new(),
+            RedisCheck::new(),
+            RedisMemoryUsageCheck::new()->failWhenAboveMb(1000),
+            ScheduleCheck::new()
+                ->heartbeatMaxAgeInMinutes(3),
         ]);
     }
 }
