73fce8f771c75ffd948dd1205cfd66f7656f0bbb
jon <jon_scott@me.com>
2026-01-26 19:58:00 +0000

    Add circle cropper to officer image upload field

--- app/Filament/Resources/Officers/Schemas/OfficerForm.php
+++ app/Filament/Resources/Officers/Schemas/OfficerForm.php
@@ -38,7 +38,8 @@ class OfficerForm
                             ->image()
                             ->directory('officers')
                             ->avatar()
-                            ->imageEditor(),
+                            ->imageEditor()
+                            ->circleCropper(),
                     ]),
             ]);
     }
