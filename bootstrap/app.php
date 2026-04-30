7841a893465a533267ebb57aec901b00a53af51d
jon <jon_scott@me.com>
2026-04-30 20:50:27 +0100

    Update Livewire configuration and Blade layout:
    - Set default `asset_url` to `/` and disabled `inject_assets` in `livewire.php`.
    - Added `@livewireStyles` and `@livewireScripts` in app layout.
    - Refactored middleware registration in `bootstrap/app.php`, including alias updates and `trustProxies` setup.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LoadSettings;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,11 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'settings' => \App\Http\Middleware\LoadSettings::class,
+            'settings' => LoadSettings::class,
         ]);
         $middleware->web(append: [
-            \App\Http\Middleware\LoadSettings::class,
+            LoadSettings::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
